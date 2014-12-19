6639fc412f37096e7e33dbde00c903b74af0bddb
Krish Rajagopalan <krish.andres@gmail.com>
2014-12-19 11:44:40 -0800

    Create users resource and add bcrypt to Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :playlists
+  resources :users
 
   root 'videos#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218194723) do
+ActiveRecord::Schema.define(version: 20141219192728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20141218194723) do
 
   add_index "playlistvideos", ["playlist_id"], name: "index_playlistvideos_on_playlist_id", using: :btree
   add_index "playlistvideos", ["video_id"], name: "index_playlistvideos_on_video_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "videos", force: true do |t|
     t.string   "title"
