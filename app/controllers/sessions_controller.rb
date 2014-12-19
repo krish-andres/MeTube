af945687a1da80ceeab33e8e132a74a700d6e1de
Krish Rajagopalan <krish.andres@gmail.com>
2014-12-19 14:02:25 -0800

    Create sessions controller and views for signin/signup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if @user = User.authenticate(params[:email], params[:password])
+      flash[:notice] = "Successfully Signed In!"
+      binding.pry
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Invalid Email/Password Combination!"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Successfully Signed Out!"
+    redirect_to root_path
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+  before_save :encrypt_password
+  validates_confirmation_of :password
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: /\A\S+@\S+\z/
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
 end
